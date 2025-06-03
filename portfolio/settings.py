b55db84466a648f7092dc1bc69af75f52296a552
NIT-GIT-346 <nithinkumar.d2005@gmail.com>
2025-06-03 10:57:09 +0530

    Nischitha-Edited the footer

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'portfolio_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
